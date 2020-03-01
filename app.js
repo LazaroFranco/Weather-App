71f8768ca869684fbd879d2f26ada28c55dfa31a
Lazaro Franco <cubanboymodz@gmail.com>
2020-02-29 23:41:16 -0500

    api fix

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "3a7097d5be18237eab64485e0a1c3aa7";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
